49a0708529323982fd5e1ebd9b5b867c507d529d
Matt Lehrer <lehrerm@gmail.com>
2023-11-02 10:48:25 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from '@cloudflare/puppeteer';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	BROWSER: Fetcher;
 }
 
+const WIDTH = 1200;
+const HEIGHT = 630;
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const { searchParams } = new URL(request.url);
+		let url = searchParams.get('url');
+		if (url) {
+			url = new URL(url).toString(); // normalize
+			const browser = await puppeteer.launch(env.BROWSER);
+			const page = await browser.newPage();
+			await page.setViewport({ width: WIDTH, height: HEIGHT, deviceScaleFactor: 1.5 });
+			await page.goto(url);
+			const img: Buffer = (await page.screenshot()) as Buffer;
+			await browser.close();
+			return new Response(img, {
+				headers: {
+					'content-type': 'image/png',
+				},
+			});
+		} else {
+			return new Response('Please add an ?url=https://example.com/ parameter');
+		}
 	},
 };
