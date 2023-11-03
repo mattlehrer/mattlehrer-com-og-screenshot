1b411e4c601d1ef1d4feb887f396505c5fd106f7
Matt Lehrer <lehrerm@gmail.com>
2023-11-03 17:01:09 +0100

    handle scrollbars with clip

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,26 @@ const HEIGHT = 630;
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
+		console.log('got request', request.url);
 		const { searchParams } = new URL(request.url);
 		let url = searchParams.get('url');
 		if (url) {
+			console.log({ url });
 			url = new URL(url).toString(); // normalize
 			const browser = await puppeteer.launch(env.BROWSER);
 			const page = await browser.newPage();
-			await page.setViewport({ width: WIDTH, height: HEIGHT, deviceScaleFactor: 1.5 });
+			await page.setViewport({ width: WIDTH + 20, height: HEIGHT + 20 });
 			await page.goto(url);
-			const img: Buffer = (await page.screenshot({ captureBeyondViewport: false })) as Buffer;
+			const img: Buffer = (await page.screenshot({
+				captureBeyondViewport: false,
+				clip: {
+					width: WIDTH,
+					height: HEIGHT,
+					x: 0,
+					y: 0,
+				},
+			})) as Buffer;
+			console.log({ img });
 			await browser.close();
 			return new Response(img, {
 				headers: {
